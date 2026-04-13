09e16bfac866f57b8b04693c16279f8060c35507
Abhishek Satish <abhishek.satish@ibm.com>
2026-04-13 16:44:49 +0530

    Update ESM wrapper to reference ibm-netezza instead of pg

--- esm/index.mjs
+++ esm/index.mjs
@@ -1,20 +1,20 @@
-// ESM wrapper for pg
-import pg from '../lib/index.js'
+// ESM wrapper for ibm-netezza
+import nz from '../lib/index.js'
 
 // Re-export all the properties
-export const Client = pg.Client
-export const Pool = pg.Pool
-export const Connection = pg.Connection
-export const types = pg.types
-export const Query = pg.Query
-export const DatabaseError = pg.DatabaseError
-export const escapeIdentifier = pg.escapeIdentifier
-export const escapeLiteral = pg.escapeLiteral
-export const Result = pg.Result
-export const TypeOverrides = pg.TypeOverrides
+export const Client = nz.Client
+export const Pool = nz.Pool
+export const Connection = nz.Connection
+export const types = nz.types
+export const Query = nz.Query
+export const DatabaseError = nz.DatabaseError
+export const escapeIdentifier = nz.escapeIdentifier
+export const escapeLiteral = nz.escapeLiteral
+export const Result = nz.Result
+export const TypeOverrides = nz.TypeOverrides
 
 // Also export the defaults
-export const defaults = pg.defaults
+export const defaults = nz.defaults
 
 // Re-export the default
-export default pg
+export default nz
